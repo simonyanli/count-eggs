188b8e0a043e61dbcee2fbfb75f6660d19431ada
simon <lixunyi@icloud.com>
2017-02-06 22:57:14 +1300

    I just analyze the characters of that number,so the code look like very easy.Because According to the question, this number should firstly be the common multiple of 7 and 9. Another important character is the last figure of this number should be 9. Because it needs more one eggs for taking 5 eggs one time without remainder.(regarding criteria 2 and 5, the figure 9 is the only answer) So this number fit two rules: 1.it is the common multiple of 9 and 7, that also means it is also the multiple of 63. 2. the last figure is 9. So just try 63 times 3, 13,23.......

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace countEggs
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int eggs;
+           for (int i=1;i<10;i++)
+            {
+                 eggs = 63 * (10 * i + 3);
+                if (((eggs+1)%5==0)&&((eggs-3)%6==0)&&((eggs-1)%8==0))
+                {
+                    Console.WriteLine("The minimum number of eggs is {0}", eggs);
+                    break;
+                }
+            }
+                
         }
     }
 }
